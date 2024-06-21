82ba6f6dfe3fc17899ddefd902d48336d39850e9
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-21 20:54:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
- const bill = document.getElementById('bill').value;
- const numOfPeople = document.getElementById('num-of-people').value;
+ const bill = document.getElementById('bill');
+ const numOfPeople = document.getElementById('num-of-people');
 
  const customTip = document.getElementById('custom-tip-btn');
- console.log(customTip)
+ 
+ let billValue = undefined;
+ let numOfPeopleValue = undefined;
+let customTipValue = undefined;
+const tipBtns = document.querySelectorAll('input[type="radio"]');
+
+
+
+ bill.addEventListener('input', () => {
+    billValue = bill.value;
+    // console.log(billValue)
+    calculateTip()
+ })
+
+ function calculateTip() {
+
+    tipBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+                tipId = btn.id; 
+                customTipValue = '';
+        })
+    })
+    
+    console.log(tipId)
+    let tip = (!customTipValue) ? tipId : customTipValue;    
+}
+
+
+ numOfPeople.addEventListener('input', () => {
+    numOfPeopleValue = numOfPeople.value;
+    calculateTip()
+ })
+
+ customTip.addEventListener('input', () => {
+     customTipValue = customTip.value;
+     console.log(customTipValue)
+    calculateTip()
+
+ })
+
+ 
+
