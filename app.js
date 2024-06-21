229a097792b9f7617215aed524c4a841265fb35b
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-21 16:19:05 +0200

    updated index.html, styles.css, app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+ const bill = document.getElementById('bill').value;
+ const numOfPeople = document.getElementById('num-of-people').value;
+
+ const customTip = document.getElementById('custom-tip-btn');
+ console.log(customTip)
