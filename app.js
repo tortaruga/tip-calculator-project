ebe1a1202dddc72944a675273ec4e8558069850a
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-22 14:05:38 +0200

    updated app.js, styles.css and index.html

--- app.js
+++ app.js
@@ -2,45 +2,100 @@
  const numOfPeople = document.getElementById('num-of-people');
 
  const customTip = document.getElementById('custom-tip-btn');
- 
+ const errorMsg = document.getElementById('error-msg');
+ const tipPerPerson = document.getElementById('tip-per-person');
+ const totalPerPerson = document.getElementById('total-per-person');
+const resetBtn = document.getElementById('reset-btn');
+
  let billValue = undefined;
  let numOfPeopleValue = undefined;
-let customTipValue = undefined;
-const tipBtns = document.querySelectorAll('input[type="radio"]');
-
+ let customTipValue = undefined;
+ let tipId = undefined;
+ let tip;
+ const tipBtns = document.querySelectorAll('input[type="radio"]');
+ 
 
 
  bill.addEventListener('input', () => {
-    billValue = bill.value;
+    billValue = Number(bill.value);
     // console.log(billValue)
-    calculateTip()
+    calculateTip();
+    enableResetBtn();
  })
 
- function calculateTip() {
+ numOfPeople.addEventListener('input', () => {
+   enableResetBtn();
 
-    tipBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-                tipId = btn.id; 
-                customTipValue = '';
-        })
-    })
-    
-    console.log(tipId)
-    let tip = (!customTipValue) ? tipId : customTipValue;    
+if (numOfPeople.value === '0') {
+  errorMsg.style.display = 'inline-block';
+  numOfPeople.style.borderColor = 'var(--error)';
+} else {
+    numOfPeopleValue = Number(numOfPeople.value);
+  errorMsg.style.display = 'none';
+  numOfPeople.style.borderColor = 'var(--strong-cyan)';
+    calculateTip();
 }
 
-
- numOfPeople.addEventListener('input', () => {
-    numOfPeopleValue = numOfPeople.value;
-    calculateTip()
  })
 
  customTip.addEventListener('input', () => {
-     customTipValue = customTip.value;
-     console.log(customTipValue)
-    calculateTip()
+     customTipValue = Number(customTip.value);
+     enableResetBtn();
+    calculateTip();
 
  })
 
+customTip.addEventListener('click', () => {
+   tipBtns.forEach(btn => {
+      btn.checked = false;
+   })
+})
+
+tipBtns.forEach(btn => {
+   btn.addEventListener('click', () => {
+    enableResetBtn();
+      tipId = Number(btn.id);
+      customTip.value = '';
+      calculateTip();
+   })
+})
+
+
+ function calculateTip() {
+if (customTipValue) {
+   tip = customTipValue;
+} else {
+   tip = tipId;
+}
+
+console.log(typeof total)
+let noTipTotalPerPerson = billValue / numOfPeopleValue;
+let individualTip = noTipTotalPerPerson * (tip / 100);
+
+tipPerPerson.innerHTML = (individualTip) ? Math.floor(individualTip * 100) / 100 : '$0.00';
+total = noTipTotalPerPerson + individualTip;
+totalPerPerson.innerHTML = total ? Math.round(total * 100) / 100 : '$0.00';
+
+}
+
+
+function disableResetBtn() {
+   resetBtn.classList.add('empty');
+}
+
+function enableResetBtn() {
+   resetBtn.classList.remove('empty');
+}
  
 
+resetBtn.addEventListener('click', () => {
+ disableResetBtn();
+ tipPerPerson.innerHTML = '$0.00';
+ totalPerPerson.innerHTML = '$0.00';
+ bill.value = '';
+ numOfPeople.value = '';
+ customTip.value = '';
+ tipBtns.forEach(btn => {
+   btn.checked = false;
+ })
+})
